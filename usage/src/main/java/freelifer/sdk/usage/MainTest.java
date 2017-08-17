48bd01ae724eec016eb8e94bb7604a3d7b34fc1d
pig <576618121@qq.com>
2017-08-18 00:49:39 +0800

    add Usage

---
+++ usage/src/main/java/freelifer/sdk/usage/MainTest.java
@@ -0,0 +1,10 @@
+package freelifer.sdk.usage;
+
+/**
+ * @author kzhu on 2017/8/17.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("args len " + args.length);
+    }
+}
--- usage/src/main/java/freelifer/sdk/usage/Usage.java
+++ usage/src/main/java/freelifer/sdk/usage/Usage.java
@@ -1,22 +1,46 @@
 package freelifer.sdk.usage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Usage {
     private final String summary;
+    private final List<Command> commands;
 
     private Usage(Builder builder) {
         this.summary = builder.summary;
+        this.commands = builder.commands;
     }
 
     public static Builder newBuilder() {
         return new Builder();
     }
 
+    public void exec(String[] args) {
+        if (args.length < 1) {
+            usage();
+            return;
+        }
+
+    }
+
+    private void usage() {
+
+    }
 
     public static class Builder {
-
         private String summary;
+        private List<Command> commands;
 
         private Builder() {
+        }
+
+        public Builder addCommand(Command command) {
+            if (commands == null) {
+                commands = new ArrayList<>();
+            }
+            commands.add(command);
+            return this;
         }
 
         public Usage build() {
